7b91bcb6e13ed84dd9ca872375b7d3d7d6db6fae
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-04-17 16:25:47 +0100

    added new paths for plot display

--- components/navbar.py
+++ components/navbar.py
@@ -5,7 +5,8 @@ from .pathindex import PathIndex
 def NavBar(pathname=None):
     return dbc.Nav([
         dbc.NavItem(dbc.NavLink("About", active=(pathname == PathIndex.HOME.value), href=PathIndex.HOME.value)),
-        dbc.NavItem(dbc.NavLink("Plot", active=(pathname == PathIndex.PLOT.value), href=PathIndex.PLOT.value)),
+        dbc.NavItem(dbc.NavLink("Plot", href=PathIndex.DATAUPLOAD.value, active=(
+                pathname == PathIndex.DATAUPLOAD.value or pathname == PathIndex.PLOTDISPLAY.value))),
         dbc.NavItem(dbc.NavLink("Contact", active=(pathname == PathIndex.CONTACT.value), href=PathIndex.CONTACT.value)),
     ],
         pills=True, fill=True, justified=True
--- components/pathindex.py
+++ components/pathindex.py
@@ -4,6 +4,7 @@ from enum import Enum
 class PathIndex(Enum):
     """An enumerator with an index of all the paths in conkit-web"""
     HOME = '/conkit-web/home/'
-    PLOT = '/conkit-web/plot/'
+    DATAUPLOAD = '/conkit-web/plot-dataupload/'
+    PLOTDISPLAY = '/conkit-web/plot-display/'
     CONTACT = '/conkit-web/contact/'
     GITHUB = 'https://github.com/rigdenlab/conkit-web'

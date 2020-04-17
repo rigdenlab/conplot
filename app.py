e6263eed965a60b96b6d344f61c33f40f74708f3
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-04-17 11:48:32 +0100

    remove unused external css and js

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import dash
 import dash_bootstrap_components as dbc
 from components import PathIndex
-from flask_caching import Cache
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX], url_base_pathname=PathIndex.HOME.value)
 app.title = 'Conkit-Web'

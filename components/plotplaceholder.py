3352f865f83b6a794c371cfe2c0629cf652a9b88
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-04-23 14:18:06 +0100

    use jumbotron for plot place holder

--- components/plotplaceholder.py
+++ components/plotplaceholder.py
@@ -1,10 +1,29 @@
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 
 def PlotPlaceHolder():
+    return dbc.Jumbotron(
+        [
+            dbc.Container(
+                [
+                    html.Img(
+                        src='https://raw.githubusercontent.com/rigdenlab/conkit-web/master/assets/conkit_small_logo.png',
+                        style={'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'}
+                    )
+                ],
+                fluid=True,
+            )
+        ],
+        fluid=True,
+    )
+
+    """
     return html.Div([
         html.Img(
             src='https://raw.githubusercontent.com/rigdenlab/conkit-web/master/assets/conkit_small_logo.png',
             style={'margin': 'auto', 'vertical-align': 'middle'}
         )
-    ], style={'display': 'flex', 'justify-content': 'center'})
+    ], style={'display': 'flex', 'justify-content': 'center'}
+    )
+    """

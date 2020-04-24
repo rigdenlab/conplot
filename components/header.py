4d9cfb0736938cd9f25435c079f5dd57f64f6a49
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-04-24 14:56:49 +0100

    add full path to header logos

--- components/header.py
+++ components/header.py
@@ -2,8 +2,8 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from index import PathIndex
 
-GITHUB_LOGO = 'assets/github_logo.png'
-CONKIT_LOGO = 'assets/conkit_small_logo.png'
+GITHUB_LOGO = '/conkit-web/home/assets/github_logo.png'
+CONKIT_LOGO = '/conkit-web/home/assets/conkit_small_logo.png'
 
 
 def github_link():

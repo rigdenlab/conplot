01bbc9521656eddc179a4a2a9ffba3392d89f300
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-05-18 13:05:35 +0100

    update SQL utils and table schema

--- postgre_sql/create_tables.sql
+++ postgre_sql/create_tables.sql
@@ -1,3 +1,23 @@
 CREATE EXTENSION pgcrypto;
-CREATE TABLE user_data (id serial primary key, username varchar(15) not null unique, email varchar(65) not null unique, password text not null);
-CREATE TABLE session_data (id serial primary key, user_id int references user_data(id), sequence bit varying, contact bit varying, membrane bit varying, secondarystructure bit varying, custom bit varying, disorder bit varying, conservation bit varying, date date not null default current_date)
+CREATE TABLE user_data
+(
+    id           serial primary key,
+    username     varchar(15) not null unique,
+    email        varchar(65) unique,
+    password     text        not null,
+    created_date date        not null,
+    last_login   date        not null
+);
+CREATE TABLE session_data
+(
+    id                 serial primary key,
+    owner_username     int references user_data (id),
+    sequence           bit varying,
+    contact            bit varying,
+    membrane           bit varying,
+    secondarystructure bit varying,
+    custom             bit varying,
+    disorder           bit varying,
+    conservation       bit varying,
+    date               date not null default current_date
+)
--- utils/sql_utils.py
+++ utils/sql_utils.py
@@ -1,5 +1,15 @@
 import os
+from enum import Enum
+import datetime
 import psycopg2
+
+
+class SqlQueries(Enum):
+    INSERT = """ INSERT INTO %s (%s) VALUES (%s)"""
+    UPDATE = """UPDATE %s SET %s = '%s' WHERE %s = '%s'"""
+    DELETE = """DELETE FROM %s WHERE %s = '%s'"""
+    LOGIN = """SELECT id FROM user_data WHERE username = '%s' AND password = crypt('%s', password)"""
+    CRYPT = """crypt('%s', gen_salt('bf'))"""
 
 
 def initiate_connection():
@@ -8,14 +18,13 @@ def initiate_connection():
     return connection, cursor
 
 
-def perform_query(query, all_results=True, commit=False):
+def perform_query(query, fetch=False, commit=False):
+    result = None
     connection, cursor = initiate_connection()
     cursor.execute(query)
 
-    if all_results:
+    if fetch:
         result = cursor.fetchall()
-    else:
-        result = cursor.fetchone()
 
     if commit:
         connection.commit()
@@ -27,33 +36,32 @@ def perform_query(query, all_results=True, commit=False):
 
 
 def insert_entry(table_name, fields, values):
-    query = """ INSERT INTO %s (%s) VALUES (%s)""" % (table_name, ",".join(fields), ",".join(values))
+    query = SqlQueries.INSERT.value % (table_name, ",".join(fields), ",".join(values))
     perform_query(query, commit=True)
 
 
-def update_entry(table_name, field, value, id):
-    query = """UPDATE %s SET %s = %s WHERE id = %s""" % (table_name, field, value, id)
+def update_entry(table_name, field_update, value_update, id_field, id_value):
+    query = SqlQueries.UPDATE.value % (table_name, field_update, value_update, id_field, id_value)
     perform_query(query, commit=True)
 
 
-def delete_entry(table_name, id):
-    query = """DELETE FROM %s WHERE id = '%s'""" % (table_name, id)
+def delete_entry(table_name, id_field, id_value):
+    query = SqlQueries.DELETE.value % (table_name, id_field, id_value)
     perform_query(query, commit=True)
 
 
-def userlogin(username, psswrd, table_name='user_data'):
-    query = """SELECT id from %s where username = '%s' AND password = crypt('%s', password)""" % (table_name, username,
-                                                                                               psswrd)
-    rslt = perform_query(query, all_results=False)
+def userlogin(username, psswrd):
+    query = SqlQueries.LOGIN.value % (username, psswrd)
 
+    rslt = perform_query(query, fetch=True)
     if rslt:
+        update_entry('user_data', 'last_login', datetime.datetime.now().strftime("%Y-%m-%d"), 'username', username)
         return True
     else:
         return False
 
 
-def create_user(username, email, psswrd, table_name='user_data', fields=('username', 'email', 'password')):
-
-    query = """INSERT INTO %s (%s) VALUES ('%s', '%s', crypt('%s', gen_salt('bf')))""" % (table_name, ",".join(fields),
-                                                                                    username, email, psswrd)
-    perform_query(query, commit=True)
+def create_user(username, email, psswrd):
+    now = datetime.datetime.now().strftime("%Y-%m-%d")
+    insert_entry('user_data', ('username', 'email', 'password', 'created_date', 'last_login'),
+                 ("'%s'" % username, "'%s'" % email, SqlQueries.CRYPT.value % psswrd, "'%s'" % now, "'%s'" % now))

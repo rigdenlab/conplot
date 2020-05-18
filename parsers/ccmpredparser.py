80d5ad1aa4818d1d6102a3f4b521f306660f5ad6
shahrammesdaghi <mesdaghi@hotmail.com>
2020-05-18 14:02:27 +0100

    ccmpred parser & test

--- parsers/ccmpredparser.py
+++ parsers/ccmpredparser.py
@@ -1,27 +1,28 @@
-import numpy as np
 from utils.exceptions import InvalidFormat
 from operator import itemgetter
 
 def CCMpredParser(input):
     contents = input.split('\n')
+
     output = []
 
-    mat = np.loadtxt(contents)
-    if mat.size > 0:
-        contacts = mat.argsort(axis=None)[::-1]
-        raw_contacts = (contacts % mat.shape[0]).astype(np.uint16), np.floor(contacts / mat.shape[0]).astype(np.uint16)
+    data = []
+    for line in contents:
+        line = line.lstrip().split('\t')
+        if not line or line[0].isalpha():
+            continue
+        else:
+            data.append(line)
 
-
-        for res1_seq, res2_seq, raw_score in zip(raw_contacts[0], raw_contacts[1], mat[raw_contacts]):
-            if res1_seq > res2_seq:
-                continue
-
-            if abs((res1_seq) - int(res2_seq)) >= 5:
-                output.append((int(res1_seq + 1), int(res2_seq + 1), float(raw_score)))
+    for res_1, score_array in enumerate(data):
+        for res_2, score in enumerate(score_array):
+            if abs((res_1+1) - int(res_2+1)) >= 5 and score!='':
+                output.append((int(res_1 + 1), int(res_2 + 1), float(score)))
 
     if not output:
         raise InvalidFormat('Unable to parse contacts')
     else:
         output = sorted(output, key=itemgetter(2), reverse=True)
         return tuple(output)
+
 

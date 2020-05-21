dfd59b6f0dff5b4c83bdc84e011e89f08629033b
shahrammesdaghi <mesdaghi@hotmail.com>
2020-05-21 09:41:19 +0100

    gremlin & colstats parser

--- parsers/comsatparser.py
+++ parsers/comsatparser.py
@@ -2,7 +2,7 @@ from utils.exceptions import InvalidFormat
 from operator import itemgetter
 
 
-def EvfoldParser(input):
+def ComsatParser(input):
     contents = input.split('\n')
     output = []
 
@@ -12,11 +12,10 @@ def EvfoldParser(input):
             continue
         elif line[0].isdigit() and line[2].isdigit() and len(line) >= 5:
             if abs(int(line[0]) - int(line[2])) >= 5:
-                output.append((int(line[0]), int(line[2]), float(line[5])))
+                output.append((int(line[0]), int(line[2]), (0)))
 
     if not output:
         raise InvalidFormat('Unable to parse contacts')
     else:
         output = sorted(output, key=itemgetter(2), reverse=True)
         return output
-
--- parsers/plmdcaparser.py
+++ parsers/plmdcaparser.py
@@ -2,17 +2,17 @@ from utils.exceptions import InvalidFormat
 from operator import itemgetter
 
 
-def EvfoldParser(input):
+def PlmdcaParser(input):
     contents = input.split('\n')
     output = []
 
     for line in contents:
-        line = line.lstrip().split()
+        line = line.lstrip().split(',')
         if not line or line[0].isalpha():
             continue
-        elif line[0].isdigit() and line[2].isdigit() and len(line) >= 5:
-            if abs(int(line[0]) - int(line[2])) >= 5:
-                output.append((int(line[0]), int(line[2]), float(line[5])))
+        elif line[0].isdigit() and line[1].isdigit():
+            if abs(int(line[0]) - int(line[1])) >= 5:
+                output.append((int(line[0]), int(line[1]), float(line[2])))
 
     if not output:
         raise InvalidFormat('Unable to parse contacts')

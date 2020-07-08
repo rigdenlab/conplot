ca0cef376edb541cd7f3e0102c07576ebeebf704
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-07-08 09:50:38 +0100

    disable ssl

--- postgresql/create_db.py
+++ postgresql/create_db.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
 
-connection = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+connection = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='disable')
 cursor = connection.cursor()
 cursor.execute(open("init.sql", "r").read())

81e0353cd3114ed01f099cd0796378eb74eaaffa
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-07-09 10:10:28 +0100

    update number of workers and manage redis config

--- static/docker/manage.sh
+++ static/docker/manage.sh
@@ -1,10 +1,11 @@
 if [ "$1" = "build" ]; then
   docker-compose up -d
   docker-compose exec web psql -U $POSTGRES_USER -d $POSTGRES_DB -h localhost -a -f postgresql/init.sql
+  docker-compose exec web redis-cli CONFIG SET maxmemory 256mb ; docker-compose exec web redis-cli CONFIG SET maxmemory-policy volatile-ttl
 elif [ "$1" = "stop" ]; then
   docker-compose down -v
 elif [ "$1" = "start" ]; then
-  sudo docker-compose -f docker-compose.yml up -d --build
+  docker-compose -f docker-compose.yml up -d --build
   docker-compose -f docker-compose.yml logs -f
 fi
 

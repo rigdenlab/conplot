7e08dc3c19b4490bcb3f2c78b1afcf2acec8802f
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-07-14 15:42:54 +0100

    expose all interfaces on port 80

--- static/docker/manage.sh
+++ static/docker/manage.sh
@@ -1,7 +1,6 @@
 if [ "$1" = "build" ]; then
   docker-compose up -d
-  docker-compose exec web psql -U $POSTGRES_USER -d $POSTGRES_DB -h localhost -a -f postgresql/init.sql
-  docker-compose exec web redis-cli CONFIG SET maxmemory 256mb ; docker-compose exec web redis-cli CONFIG SET maxmemory-policy volatile-ttl
+  docker-compose exec web psql -U $POSTGRES_USER -d $POSTGRES_DB -h postgres_db -a -f postgresql/init.sql
 elif [ "$1" = "stop" ]; then
   docker-compose down -v
 elif [ "$1" = "start" ]; then

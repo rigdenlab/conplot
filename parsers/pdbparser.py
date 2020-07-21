cc74707f11f9edeb621e3c249890303bd79861b1
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-07-21 18:07:38 +0100

    use gemmmi to trim first chain of PDB files

--- parsers/pdbparser.py
+++ parsers/pdbparser.py
@@ -1,14 +1,25 @@
 import io
 import conkit.io
+import gemmi
 from operator import itemgetter
 from utils import unique_by_key
 from utils.exceptions import InvalidFormat
 
 
+def get_first_chain(structure):
+    new_model = gemmi.Model('1')
+    new_model.add_chain(structure[0][0].clone())
+    new_structure = gemmi.Structure()
+    new_structure.add_model(new_model)
+    return new_structure.make_minimal_pdb()
+
+
 def PDBParser(input):
     try:
+        structure = gemmi.read_pdb_string(input)
+        input_chain = get_first_chain(structure)
         parser = conkit.io.PARSER_CACHE.import_class('pdb')()
-        cmap = parser.read(f_handle=io.StringIO(input))
+        cmap = parser.read(f_handle=io.StringIO(input_chain))
         cmap = cmap.top_map
         cmap.remove_neighbors(inplace=True)
         cmap.sort('raw_score', reverse=True, inplace=True)

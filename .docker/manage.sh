74aec1f422f8222cea10c4aa3b07cb492253d088
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-11-18 09:55:57 +0000

    added .docker

---
+++ .docker/manage.sh
@@ -0,0 +1,10 @@
+if [ "$1" = "build" ]; then
+  docker-compose up -d
+  docker-compose exec web psql -U $POSTGRES_USER -d $POSTGRES_DB -h postgres_db -a -f postgresql/init.sql
+elif [ "$1" = "stop" ]; then
+  docker-compose down -v
+elif [ "$1" = "start" ]; then
+  docker-compose -f docker-compose.yml up -d --build
+  docker-compose -f docker-compose.yml logs -f
+fi
+

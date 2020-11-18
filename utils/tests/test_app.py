e417adffe5e3da0a61d14d7d1b154844f83feffc
FilomenoSanchez <filo_san_rod@hotmail.com>
2020-11-18 10:51:56 +0000

    build only if push master and test pass

--- utils/tests/test_app.py
+++ utils/tests/test_app.py
@@ -7,7 +7,7 @@ class AppTestCase(unittest.TestCase):
     def test_1(self):
         stylesheets = ['https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/lux/bootstrap.min.css',
                        'https://use.fontawesome.com/releases/v5.7.2/css/all.css']
-        self.assertEqual('ConPlot_TEST', app.title)
+        self.assertEqual('ConPlot', app.title)
         self.assertListEqual(stylesheets, app.config['external_stylesheets'])
 
 

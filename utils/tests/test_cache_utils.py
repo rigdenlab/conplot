9b90bd17b23a8834d2d7361ec33cf572e8c31f79
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-02-11 10:32:15 +0000

    updated test_cache_utils.py

--- utils/tests/test_cache_utils.py
+++ utils/tests/test_cache_utils.py
@@ -1,6 +1,7 @@
 import unittest
 import keydb
-from utils import cache_utils, keydb_utils
+import logging
+from utils import cache_utils, keydb_utils, session_utils
 
 
 class CacheUtilsTestCase(unittest.TestCase):
@@ -9,6 +10,7 @@ class CacheUtilsTestCase(unittest.TestCase):
     def setUpClass(cls):
         keydb_pool = keydb_utils.create_pool('redis://127.0.0.1:6379')
         cls.cache = keydb.KeyDB(connection_pool=keydb_pool)
+        cls.session_id = session_utils.initiate_session(cls.cache, logging.getLogger())
 
     def test_1(self):
         dummy_data = [x for x in range(0, 100)]
@@ -24,3 +26,53 @@ class CacheUtilsTestCase(unittest.TestCase):
 
     def test_3(self):
         self.assertTrue(cache_utils.is_redis_available(self.cache))
+
+    def test_4(self):
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname_1', 'dummy_key')
+        self.assertTrue(self.cache.hexists(self.session_id, 'dummy_key'))
+        expected = cache_utils.compress_data(['dummy_fname_1'])
+        self.assertEqual(self.cache.hget(self.session_id, 'dummy_key'), expected)
+
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname_2', 'dummy_key')
+        expected = cache_utils.compress_data(['dummy_fname_1', 'dummy_fname_2'])
+        self.assertEqual(self.cache.hget(self.session_id, 'dummy_key'), expected)
+        self.cache.hdel(self.session_id, 'dummy_key')
+
+    def test_5(self):
+        self.assertIsNone(cache_utils.remove_fname(self.cache, self.session_id, 'dummy_fname', 'dummy_key'))
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname_1', 'dummy_key')
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname_2', 'dummy_key')
+
+        cache_utils.remove_fname(self.cache, self.session_id, 'dummy_fname_1', 'dummy_key')
+        expected = cache_utils.compress_data(['dummy_fname_2'])
+        self.assertEqual(self.cache.hget(self.session_id, 'dummy_key'), expected)
+
+        cache_utils.remove_fname(self.cache, self.session_id, 'dummy_fname_2', 'dummy_key')
+        expected = cache_utils.compress_data([])
+        self.assertEqual(self.cache.hget(self.session_id, 'dummy_key'), expected)
+        self.cache.hdel(self.session_id, 'dummy_key')
+
+    def test_6(self):
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname', 'sequence')
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname', 'hydro')
+        cache_utils.remove_fname(self.cache, self.session_id, 'dummy_fname', 'sequence')
+        self.assertIsNone(self.cache.hget(self.session_id, 'sequence'))
+        self.assertIsNone(self.cache.hget(self.session_id, 'hydro'))
+
+    def test_7(self):
+        cache_utils.store_figure(self.session_id, 'figure_json', 'display_control_json', self.cache)
+        expected = cache_utils.compress_data('figure_json')
+        self.assertEqual(self.cache.hget(self.session_id, 'figure_json'), expected)
+        expected = cache_utils.compress_data('display_control_json')
+        self.assertEqual(self.cache.hget(self.session_id, 'display_control_json'), expected)
+
+    def test_8(self):
+        self.cache.hset(self.session_id, 'dummy_fname', 'sequence_data')
+        self.cache.hset(self.session_id, 'hydro', 'seq_hydrophobicity')
+        self.cache.hset(self.session_id, 'sequence', cache_utils.compress_data('dummy_fname'))
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname_1', 'contact')
+        cache_utils.store_fname(self.cache, self.session_id, 'dummy_fname_2', 'disorder')
+        cache_utils.store_figure(self.session_id, 'figure_json', 'display_control_json', self.cache)
+        cache_utils.clear_cache(self.session_id, self.cache)
+        expected = {b'id': cache_utils.compress_data(self.session_id)}
+        self.assertDictEqual(expected, self.cache.hgetall(self.session_id))

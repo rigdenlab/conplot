1df088b96f2c0c1e47b4e744e4c06ec041ea7680
FilomenoSanchez <filo_san_rod@hotmail.com>
2021-04-26 17:22:36 +0100

    skip conkit density test if running on github actions

--- utils/tests/test_tracks_utils.py
+++ utils/tests/test_tracks_utils.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from utils import tracks_utils
 from collections import namedtuple
@@ -7,6 +8,7 @@ DisplayControlSettings = namedtuple('DisplayControlSettings', ('factor', 'seq_le
 
 class TrackUtilsTestCase(unittest.TestCase):
 
+    @unittest.skipIf('THIS_IS_GH_ACTIONS' in os.environ, "not implemented in Travis CI")
     def test_1(self):
         dummy_cmap = [(52, 50), (53, 51), (145, 143), (142, 140), (150, 148), (53, 50), (147, 145), (141, 139),
                       (143, 141), (148, 146)]
@@ -63,3 +65,38 @@ class TrackUtilsTestCase(unittest.TestCase):
 
         diff = tracks_utils.calculate_diff(dummy_cmap_1, dummy_cmap_2, dummy_display_settings)
         self.assertListEqual(diff, expected_diff)
+
+    def test_5(self):
+        """
+        cmap_1
+            2 8 9 0
+            5 0 0 9
+            7 0 0 8
+            0 7 5 2
+        cmap_2
+            9 6 0 0
+            5 1 0 0
+            5 0 1 6
+            0 5 5 9
+        """
+        cmap_1 = [
+            [2, 1, 0, 7],
+            [3, 1, 0, 5],
+            [4, 1, 0, 2],
+            [4, 2, 0, 8],
+            [3, 2, 0, 0],
+            [4, 3, 0, 9]
+        ]
+
+        cmap_2 = [
+            [2, 1, 0, 5],
+            [3, 1, 0, 5],
+            [4, 1, 0, 9],
+            [3, 2, 0, 1],
+            [4, 2, 0, 6],
+            [4, 3, 0, 0]
+        ]
+
+        expected = [7, 4, 10, 10]
+        output = tracks_utils.get_cmap_rmsd(cmap_1, cmap_2, 4)
+        self.assertListEqual(output, expected)
